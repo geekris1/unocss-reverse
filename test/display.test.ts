1d489049237c92cb23285cbd9661ebe2f4fd9e3f
geekris1 <200949970@qq.com>
2023-02-25 23:52:33 +0800

    test(display):add more test example

--- test/display.test.ts
+++ test/display.test.ts
@@ -6,11 +6,38 @@ describe('display', () => {
     expect(generator('display: block')).toBe('block')
   })
 
-  it('display: none', () => {
-    expect(generator('display: none')).toBe('hidden')
+  it('display: inline', () => {
+    expect(generator('display: inline')).toBe('inline')
   })
 
   it('display: inline-block', () => {
     expect(generator('display: inline-block')).toBe('inline-block')
+  })
+
+  it('display: flex', () => {
+    expect(generator('display: flex')).toBe('flex')
+  })
+
+  it('display: inline-flex', () => {
+    expect(generator('display: inline-flex')).toBe('inline-flex')
+  })
+
+  it('display: grid', () => {
+    expect(generator('display: grid')).toBe('grid')
+  })
+
+  it('display: inline-grid', () => {
+    expect(generator('display: inline-grid')).toBe('inline-grid')
+  })
+
+  it('display: flow-root', () => {
+    expect(generator('display: flow-root')).toBe('flow-root')
+  })
+
+  it('display: contents', () => {
+    expect(generator('display: contents')).toBe('contents')
+  })
+  it('display: none', () => {
+    expect(generator('display: none')).toBe('hidden')
   })
 })
